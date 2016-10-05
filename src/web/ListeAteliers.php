f636cf664c4770245a26cb0113e958808e1c334b
Thomas VIGUÉ <thomas.vigue@etu.u-bordeaux.fr>
2016-10-05 02:36:25 +0200

    T3.1 Liste Ateliers Done + Update Kanban

--- src/web/ListeAteliers.php
+++ src/web/ListeAteliers.php
@@ -7,18 +7,18 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Liste ateliers</title>
+    <title>Liste des ateliers</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
+    <!-- <link href="css/starter-template.css" rel="stylesheet"> -->
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>
+    <!-- <script src="js/ie-emulation-modes-warning.js"></script> -->
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -51,68 +51,98 @@
 
     <div class="container">
       <div class="starter-template">
-        <h1>Liste ateliers</h1>
-        <table class="table_ateliers">
+        <table style="width: 100%;">
+          <tr>
+            <td><h1>Liste ateliers</h1></td>
+            <td style="text-align: right;"><a class="btn btn-default" href="FormulaireAtelier.php" role="button">Creer un atelier</a></td>
+          </tr>
+        </table>
+        <table class="table table-bordered">
+           <thead>
+            <tr>
+              <th>Titre</th>
+              <th>Thème</th>
+              <th>Type</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+
 <?php
 
-// connexion à la base de données
-$db = new mysqli('localhost','root','','projet_cdp2016_acv');
+  // connexion à la base de données
+  $db = new mysqli('localhost','root','','projet_cdp2016_acv');
 
-if($db->connect_errno) {
-  printf("Echec de la connexion: %s\n", $db->connect_error);
-  exit();
-}
-
-// calcule du nombre de pages
-$article_par_pages=20;
-
-$query_nombre_ateliers="SELECT * FROM Ateliers";
-
-if($result=$db->query($query_nombre_ateliers)) {
-  $nombre_lignes=mysqli_num_rows($result);
-  $nombre_de_pages=ceil($nombre_lignes/$article_par_pages);
-
-  $result->close(); // libération du jeu de résultats
-}
-
-// affichage des ateliers de la page en cours
-if(isset($_GET['page'])) {
-  $page_actuelle=intval($_GET['page']);
-
-  if($page_actuelle>$nombre_de_pages) {
-    $page_actuelle=$nombre_de_pages;
-  }
-} else {
-  $page_actuelle=1;
-}
-
-$id_premiere_ligne=($page_actuelle-1)*$article_par_pages;
-
-$query_ateliers="SELECT * FROM Ateliers AS A ORDER BY A.date_atelier ASC LIMIT ".$id_premiere_ligne.", ".$article_par_pages;
-
-if($result=$db->query($query_ateliers)) {
-  while ($row=$result->fetch_assoc()) {
-    printf("%d %s %s %s %d %s %s %s %s %d %d<br>\n", $row["id_atelier"], $row["nom_atelier"], $row["theme_atelier"], $row["type_atelier"], $row["id_laboratoire"], $row["lieu_atelier"], $row["duree_atelier"], $row["date_atelier"], $row["horaire_atelier"], $row["capacite_atelier"], $row["inscription_atelier"]);
+  if($db->connect_errno) {
+    printf("Echec de la connexion: %s\n", $db->connect_error);
+    exit();
   }
 
-  $result->close();
-}
+  // calcule du nombre de pages
+  $article_par_pages=20;
 
-$db->close();  // on ferme la connexion
+  $query_nombre_ateliers="SELECT * FROM Ateliers";
 
-// affichage de la liste des pages
-printf("<p align=\"center\">\nPage : ");
-for($i=1; $i<=$nombre_de_pages; $i++) {
-  if($i==$page_actuelle) {
-    printf(" [ %d ] \n", $i);
+  if($result=$db->query($query_nombre_ateliers)) {
+    $nombre_lignes=mysqli_num_rows($result);
+    $nombre_de_pages=ceil($nombre_lignes/$article_par_pages);
+
+    $result->close(); // libération du jeu de résultats
+  }
+
+  // affichage des ateliers de la page en cours
+  if(isset($_GET['page'])) {
+    $page_actuelle=intval($_GET['page']);
+
+    if($page_actuelle>$nombre_de_pages) {
+      $page_actuelle=$nombre_de_pages;
+    }
   } else {
-    printf(" <a href=\"ListeAteliers.php?page=%d\">%d</a> \n", $i, $i);
+    $page_actuelle=1;
   }
-}
-printf("</p>\n");
+
+  $id_premiere_ligne=($page_actuelle-1)*$article_par_pages;
+
+  $query_ateliers="SELECT * FROM Ateliers AS A ORDER BY A.date_atelier ASC LIMIT ".$id_premiere_ligne.", ".$article_par_pages;
+
+  if($result=$db->query($query_ateliers)) {
+    while ($row=$result->fetch_assoc()) {
+      // printf("%d %s %s %s %d %s %s %s %s %d %d<br>\n", $row["id_atelier"], $row["nom_atelier"], $row["theme_atelier"], $row["type_atelier"], $row["id_laboratoire"], $row["lieu_atelier"], $row["duree_atelier"], $row["date_atelier"], $row["horaire_atelier"], $row["capacite_atelier"], $row["inscription_atelier"]);
+      printf("            <tr>\n");
+      printf("              <td>%s</td><td>%s</td><td>%s</td>\n", $row["nom_atelier"], $row["theme_atelier"], $row["type_atelier"]);
+      printf("              <td>\n");
+      printf("                <form style=\"display: inline;\" action=\"FormulaireAtelier.php\" method=\"post\">\n");
+      printf("                  <input type=\"hidden\" name=\"id_atelier\"value=\"%s\"/>\n", $row["id_atelier"]);
+      printf("                  <input class=\"btn btn-default\" type=\"submit\" value=\"Modifier\"/>\n");
+      printf("                </form>\n");
+      printf("                <form style=\"display: inline;\" action=\"#\" method=\"post\">\n");
+      printf("                  <input type=\"hidden\" name=\"id_atelier\"value=\"%s\"/>\n", $row["id_atelier"]);
+      printf("                  <input class=\"btn btn-default\" type=\"submit\" value=\"Suprimer\"/>\n");
+      printf("                </form>\n");
+      printf("              </td>\n");
+      printf("            </tr>\n");
+    }
+
+    $result->close();
+  }
+
+  $db->close();
+
+  printf("\t\t  </tbody>\n\t\t</table>\n");
+
+  // affichage de la liste des pages
+  printf("<p style=\"text-align:center\">\nPage : ");
+  for($i=1; $i<=$nombre_de_pages; $i++) {
+    if($i==$page_actuelle) {
+      printf(" [ %d ] \n", $i);
+    } else {
+      printf(" <a href=\"ListeAteliers.php?page=%d\">%d</a> \n", $i, $i);
+    }
+  }
+  printf("</p>\n");
 
 ?>
-        </table>
+
       </div>
     </div><!-- /.container -->
 

6f144be7d34eba77bf3acce32fe9566826c502cb
Nathalie Craeye <nathalie.craeye@laposte.net>
2016-10-06 03:09:36 +0200

    Tests BDD a executer UNE SEULE FOIS sur la table Ateliers sinon il y aura violation de la contrainte sur la primary key 101

--- src/bd/testsBD_traitements.php
+++ src/bd/testsBD_traitements.php
@@ -1,6 +1,5 @@
 <?php
-	// Pour afficher les caractères accentués
-	ini_set('default_charset', 'utf8');
+	ini_set('default_charset', 'utf8'); // Pour afficher les caractères accentués
 
 	function getmicrotime() {
 		list($usec, $sec) = explode(" ",microtime());
@@ -11,7 +10,7 @@
 	$DBhost  = "localhost";  // serveur de la bdd
 	$DBowner = "root";  // login SQL
 	$DBpwd    = "";  // password SQL
-	$DBname  = "projet_cdp2016_acv";
+	$DBname  = "projet_cdp2016_acv"; // nom de la bdd
 
 	
 	echo '<h1>Tests sur la base de données : '.$DBname.'. </h1><br />';
@@ -23,7 +22,7 @@
 		$bdd = new PDO('mysql:host='.$DBhost.';dbname='.$DBname.';charset=utf8', $DBowner, $DBpwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}
 	catch (Exception $e) {
-		die('ERREUR de connexion :' . $e->getMessage());
+		die('ERREUR de connexion :' . $e->getMessage().'<br>');
 	}
 
 	echo '- La connexion à la bdd est fonctionnelle ! <br>  <br>';
@@ -31,14 +30,14 @@
 	
 	$Table_SQL = "Table_test";
 
-	/******* Supression d'une table *******/
+	/******* Suppression d'une table *******/
 	try {
 	$suppr = $bdd->query('DROP TABLE IF EXISTS `'.$Table_SQL.'`');
 	}
 	catch (Exception $e) {
-		die('ERREUR de DROP :' . $e->getMessage());
+		die('ERREUR de DROP :' . $e->getMessage().'<br>');
 	}
-	echo '- Supression d\'une table dans la bdd <b>'.$DBname.'</b> ok !<br>  <br>';
+	echo '- Suppression d\'une table dans la bdd <b>'.$DBname.'</b> ok !<br>  <br>';
 
 
 	/******* Création d'une table *******/
@@ -46,7 +45,7 @@
 	$suppr = $bdd->query('CREATE TABLE `'.$Table_SQL.'` (`Id` int(11) NOT NULL auto_increment,  `NOM` varchar(32) NOT NULL default \'\',  `PRENOM` varchar(32) NOT NULL default \'\', PRIMARY KEY  (`Id`)) ENGINE=MyISAM DEFAULT CHARSET=utf8');
 	}
 	catch (Exception $e) {
-		die('ERREUR de CREATE :' . $e->getMessage());
+		die('ERREUR de CREATE :' . $e->getMessage().'<br>');
 	}
 	echo '- Création d\'une table dans la bdd <b>'.$DBname.'</b> ok !<br>  <br>';  
 
@@ -57,7 +56,7 @@
 		$ajout2 = $bdd->query('INSERT INTO `'.$Table_SQL.'`  VALUES (\'\',\'accents-éèàâ\',\'Jean-Noël\')');
 	}
 	catch (Exception $e) {
-		die('ERREUR d\'INSERT INTO :' . $e->getMessage());
+		die('ERREUR d\'INSERT INTO :' . $e->getMessage().'<br>');
 	}
 	
 	// Modification d'un élément
@@ -68,7 +67,7 @@
 		die('ERREUR  d\'UPDATE : '. $e->getMessage());
 	}
 
-	echo '- Modification d\'une table dans la bdd <b>'.$DBname.'</b> ok !<br>  <br><br>  <br>';
+	echo '- Modification d\'une table dans la bdd <b>'.$DBname.'</b> ok !<br>  <br><br>';
   
   
 	echo '<h1>Tests des requêtes sur la table : Ateliers. </h1><br />';
@@ -87,11 +86,11 @@
 	}
 	$selectAll->closeCursor(); 	// On termine le traitement
 	$rowCount_getAll = $selectAll->rowCount(); 
-	if ($rowCount_getAll <= 100) {
-		die('ERREUR de SELECT sur la table Ateliers : ($rowCount_getAll='.$rowCount_getAll.') < 100');
+	if ($rowCount_getAll < 100) {
+		die('ERREUR de SELECT sur la table Ateliers : ($rowCount_getAll='.$rowCount_getAll.') < 100 <br>');
 	}
 	$Date_end = getmicrotime();
-	echo '- Requête de récupération des 100 ateliers : ok !<br>';
+	echo '- Requête de récupération des 100 ateliers : ok !<br> <br>';
 
 	/******* Affichage de 5 éléments *******/
 	// On récupère la partie de la table dont id_laboratoire = 89
@@ -99,19 +98,19 @@
 	$selectByLabo = $bdd->query('SELECT * FROM Ateliers AS At WHERE id_laboratoire='.$id_labo.' ORDER BY At.date_atelier ASC');
 	$result_count_row_get = $selectByLabo->rowCount(); 
 	if ($result_count_row_get != 5)	{
-		die('ERREUR de SELECT partiel sur la table Ateliers : ($result_count_row_get='.$result_count_row_get.') != 5');
+		die('ERREUR de SELECT partiel sur la table Ateliers : ($result_count_row_get='.$result_count_row_get.') != 5 <br>');
 	}
-	echo '- Requête de récupération des 5 ateliers du laboratoire n°'.$id_labo.' : ok !<br>';
+	echo '- Requête de récupération des 5 ateliers du laboratoire n°'.$id_labo.' : ok !<br> <br>';
 	
 	
 	/******* Insertion d'un élément *******/
-	$insertInto = $bdd->query('INSERT INTO Ateliers (nom_atelier,theme_atelier,type_atelier,id_laboratoire,lieu_atelier,duree_atelier,date_atelier,horaire_atelier,capacite_atelier,inscription_atelier) VALUES ("$nom","$theme","$type",102,"$lieu","$duree","00:41:44","20:41:44",20,0)');
-	$selectById = $bdd->query('SELECT * FROM Ateliers WHERE id_atelier=102');
+	$insertInto = $bdd->query('INSERT INTO Ateliers (`id_atelier`, `nom_atelier`, `theme_atelier`, `type_atelier`, `id_laboratoire`, `lieu_atelier`, `duree_atelier`, `date_atelier`, `horaire_atelier`, `capacite_atelier`, `inscription_atelier`) VALUES (101,"[value-2]","[value-3]","[value-4]",101,"[value-6]","[value-7]","[value-8]","[value-9]",10,11)');
+	$selectById = $bdd->query('SELECT * FROM Ateliers WHERE id_atelier=101');
 	$result_count_row_get = $selectById->rowCount(); 
 	if ($result_count_row_get != 1)	{
-		die('ERREUR d\'insertion dans la table Ateliers : ($result_count_row_get='.$result_count_row_get.') != 1');
+		die('ERREUR d\'insertion dans la table Ateliers : ($result_count_row_get='.$result_count_row_get.') != 1 <br>');
 	}
-	echo '- Requête d\'insertion d\'un atelier dans la table : ok !<br>';
+	echo '- Requête d\'insertion d\'un atelier dans la table : ok !<br> <br>';
 	
 	
 	/******* Suppression d'un élément *******/
@@ -122,21 +121,20 @@
 	if ($result_count_row_get != 0) {
 		die('ERREUR de suppression dans la table Ateliers : ($result_count_row_get='.$result_count_row_get.') != 0 <br>');
 	}
-	echo '- Requête de suppression d\'un atelier dans la table : ok !<br>';
+	echo '- Requête de suppression d\'un atelier dans la table : ok !<br> <br>';
 
 	/******* Modification d'un élément *******/
-	$update = $bdd->query('UPDATE Ateliers SET nom_atelier = "$nom", theme_atelier="$theme", type_atelier="$type", lieu_atelier="$lieu", duree_atelier="$duree", date_atelier="$date", horaire_atelier="$horaire", capacite_atelier=10, inscription_atelier=1 WHERE id_atelier = 17 AND id_laboratoire = 89');
+	$update = $bdd->query('UPDATE Ateliers SET nom_atelier = "nom", theme_atelier="theme", type_atelier="type", lieu_atelier="lieu", duree_atelier="duree", date_atelier="date", horaire_atelier="horaire", capacite_atelier=10, inscription_atelier=1 WHERE id_atelier = 17 AND id_laboratoire = 89');
 	$selectByIds = $bdd->query('SELECT * FROM Ateliers WHERE id_atelier=17 AND id_laboratoire=89');
 	$rep = $selectByIds->fetch();
-	if ($rep['id_atelier']==17 && $rep['nom_atelier']=="$nom" && $rep['theme_atelier']=="$theme" && $rep['type_atelier']=="$type" && $rep['id_laboratoire']==89 && $rep['lieu_atelier']=="$lieu" && $rep['duree_atelier']=="$duree" && $rep['date_atelier']=="$date" && $rep['horaire_atelier']="$horaire" && $rep['capacite_atelier']=="$capacite" && $rep['inscription_atelier']==1)
-	{
-		die('ERREUR de modification dans la table Ateliers.');
+	if ($rep['id_atelier']==17 && $rep['nom_atelier']=="nom" && $rep['theme_atelier']=="theme" && $rep['type_atelier']=="type" && $rep['id_laboratoire']==89 && $rep['lieu_atelier']=="lieu" && $rep['duree_atelier']=="duree" && $rep['date_atelier']=="date" && $rep['horaire_atelier']="horaire" && $rep['capacite_atelier']==10 && $rep['inscription_atelier']==1) {
+		die('ERREUR de modification dans la table Ateliers. <br>');
 	}
-	echo '- Requête de modification d\'un atelier dans la table : ok !<br>  <br><br>  <br>';
+	echo '- Requête de modification d\'un atelier dans la table : ok !<br><br><br>';
 
 	
-	echo "<h1> Les performances </h1><br />";
+	echo "<h1>Tests de performance</h1><br />";
 	$Duree = $Date_end - $Date_start;
 	$Duree = sprintf("%01.2f", $Duree);
-	echo " <br>  Le traitement des 100 données de cette BDD a une durée égale à <b>$Duree</b> secondes. <br> \n";
+	echo "Le traitement des 100 données de cette BDD a une durée égale à <b>$Duree</b> secondes. <br>";
 ?>

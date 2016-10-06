770225b1da3b566fae4fd647be3de20b6d85120f
tautret <autret.tristan@gmail.com>
2016-10-06 23:40:45 +0200

    Modificate Atelier and location issues solved

--- src/web/FormulaireAtelier.php
+++ src/web/FormulaireAtelier.php
@@ -1,102 +1,125 @@
-<?php?>
+<?php
+if(isset($_POST['id_atelier']))      $id_atelier=$_POST['atelier'];
+$id_atelier = 0;
+// connexion à la base
+$db = new mysqli('localhost','root','','project');
+
+if($db->connect_errno){
+  printf("Echec de la connexion: %s\n", $db->connect_error);
+  exit();
+}
+
+$sql ="SELECT * FROM Ateliers where id_atelier = '$id_atelier'";
+if(!$result = $db->query($sql)){
+  printf("Message d'erreur : %s \n", $db->error);
+}
+$row = $result->fetch_assoc();
+?>
 <!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <title>Création atelier</title>
+  <title>Starter Template for Bootstrap</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/custom.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="starter-template.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>
+  <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+  <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+  <script src="js/ie-emulation-modes-warning.js"></script>
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
 
-  <body>
+<body>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-
+  <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Project name</a>
+      </div>
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
 
-      <div class="starter-template">
-        <h1>Création d'un atelier</h1>
-        <form action="create.php" method="post">
+  <div class="container">
+
+    <div class="starter-template">
+      <h1>Création atelier</h1>
+      <form action="create.php" method="post" >
         <table class="table_formulaire">
           <tr>
-            <td>Nom de l'atelier</td>
-            <td><input type="text" name="nom" /></td>
+            <td>Nom de l'Atelier</td>
+            <td><input type="text" name="nom" value="<?php if(isset($row['name_atelier'])) echo $row['name_atelier'];?>" /></td>
           </tr>
           <tr>
-            <td>Thème</td>
-            <td><input type="text" name="theme" /></td>
+            <td>Theme</td>
+            <td><input type="text" name="theme" value="<?php if(isset($row['theme_atelier'])) echo $row['theme_atelier'];?>" /></td>
           </tr>
           <tr>
             <td>Type</td>
-            <td><input type="text" name="type" /></td>
+            <td><input type="text" name="type" value="<?php if(isset($row['type_atelier'])) echo $row['type_atelier'];?>" /></td>
           </tr>
           <tr>
             <td>Lieu</td>
-            <td><input type="text" name="lieu" value="<?php if(isset($test)) echo $test;?>" /></td>
+            <td><input type="text" name="lieu" value="<?php if(isset($row['lieu_atelier'])) echo $row['lieu_atelier'];?>" /></td>
           </tr>
           <tr>
             <td>Durée(HH:MM)</td>
-            <td><input type="time" name="duree" required/></td>
+            <td><input type="time" name="duree" value="<?php if(isset($row['duree_atelier'])) echo $row['duree_atelier'];?>" required/></td>
           </tr>
           <tr>
             <td>Date</td>
-            <td><input type="date" name="date" required/></td>
+            <td><input type="date" name="date" value="<?php if(isset($row['date_atelier'])) echo $row['date_atelier'];?>" required/></td>
           </tr>
           <tr>
             <td>Horaire</td>
-            <td><input type="time" name="horaire" required/></td>
+            <td><input type="time" name="horaire" value="<?php if(isset($row['horaire_atelier'])) echo $row['horaire_atelier'];?>" required/></td>
           </tr>
           <tr>
-            <td>Nombre de places</td>
-            <td><input type="number" name="capacite" min="0" value="1")/></td>
+            <td>Nb de place</td>
+            <td><input type="number" name="capacite" min="0" value="1" value="<?php if(isset($row['capacite'])) echo $row['capacite'];?>" )/></td>
           </tr>
           <tr>
             <td>Inscription</td>
-            <td><input type="checkbox" name="inscription"/></td>
+            <td><input type="checkbox" name="inscription" value="<?php if(isset($row['inscription'])) echo $row['inscription'];?>" /></td>
           </tr>
           <tr>
-            <td colspan="2"><input type="submit" value="Ajouter" name="ajouter"></td>
-        </table>
-      </form>
+            <?php
+            if(isset($id_atelier)){
+              echo "<td colspan='2'><input type='submit' value='Modifier' name='modifier'></td>";
+              echo "<input type=hidden name='id_atelier' value='$id_atelier'";
+            } else {
+              echo "<td colspan='2'><input type='submit' value='Ajouter' name='ajouter'></td>";
+            }
+            ?>
+          </table>
+        </form>
       </div>
 
     </div><!-- /.container -->
@@ -108,4 +131,4 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
-</html>
+  </html>
--- src/web/create.php
+++ src/web/create.php
@@ -1,64 +1,64 @@
 <?php
-	// connexion à la base
-	$db = new mysqli('localhost','root','','projet_cdp2016_acv'); //BDD chez nous
-  //$db = new mysqli('dbserver','<LOGIN>','<MOT DE PASSE>','ncraeye'); //BDD à choisir si connexion au Cremi
 
-  if($db->connect_errno){
-  	die("Echec de la connexion: ".$db->connect_error."<br>");
-  	exit();
-  }
+// connexion à la base
+$db = new mysqli('localhost','root','','project');
 
+if($db->connect_errno){
+  printf("Echec de la connexion: %s\n", $db->connect_error);
+  exit();
+}
 // On commence par récupérer les champs
-  if(isset($_POST['nom']))  $nom=$_POST['nom'];
-  else  $nom="N";
 
-  if(isset($_POST['theme']))  $theme=$_POST['theme'];
-  else  $theme="N";
+if(isset($_POST['nom']))      $nom=$_POST['nom'];
 
-  if(isset($_POST['type']))  $type=$_POST['type'];
-  else  $type="N";
+if(isset($_POST['theme']))      $theme=$_POST['theme'];
 
-  if(isset($_POST['lieu']))  $lieu=$_POST['lieu'];
-  else  $lieu="N";
+if(isset($_POST['type']))      $type=$_POST['type'];
 
-  if(isset($_POST['id_laboratoire']))  $id_laboratoire=$_POST['id_laboratoire'];
-  else  $id_laboratoire=1;
+if(isset($_POST['lieu']))      $lieu=$_POST['lieu'];
 
-  if(isset($_POST['date']))  $date= mysqli_real_escape_string($db,$_POST['date']);
-  else $date="2016-10-07";
+if(isset($_POST['id_laboratoire']))      $id_laboratoire=$_POST['id_laboratoire'];
+else      $id_laboratoire=1;
 
-  if(isset($_POST['duree']))  $duree=$_POST['duree'];
-  else  $duree='00:00';
+if(isset($_POST['duree']))      $duree=$_POST['duree'];
 
-  if(isset($_POST['horaire']))  $horaire=$_POST['horaire'];
-  else  $horaire='00:00';
+if(isset($_POST['horaire']))      $horaire=$_POST['horaire'];
 
-  if(isset($_POST['capacite']))  $capacite=$_POST['capacite'];
-  else $capacite=0;
+if(isset($_POST['date']))      $date= mysqli_real_escape_string($db,$_POST['date']);
 
-  if(isset($_POST['inscription']))  $inscription=1;
-  else $inscription=0;
+if(isset($_POST['capacite']))      $capacite=$_POST['capacite'];
+
+if(isset($_POST['inscription']))      $inscription=1;
+else $inscription =0;
 
 // on écrit la requête sql
-  $id = $db->query("SELECT MAX(id_atelier) FROM ateliers;");
-  while ($result = $id->fetch_assoc())
-  {
-  	if($result['MAX(id_atelier)'] == NULL){
-  		$id_atelier = 0;
-  	} else {
-  		$id_atelier = (int)$result['MAX(id_atelier)'];
-  		++$id_atelier;
-  	}
+$id = $db->query("SELECT MAX(id_atelier) FROM ateliers;");
+while ($result = $id->fetch_assoc())
+{
+  if($result['MAX(id_atelier)'] == NULL){
+    $id_atelier = 0;
+  } else {
+    $id_atelier = (int)$result['MAX(id_atelier)'];
+    ++$id_atelier;
   }
-  echo($id_atelier);
-
-  $sql ="INSERT INTO ateliers (id_atelier,nom_atelier,theme_atelier,type_atelier,id_laboratoire,lieu_atelier,duree_atelier,date_atelier,horaire_atelier,capacite_atelier,inscription_atelier)
+}
+// Update Atelier //
+if(isset($_POST['id_atelier'])){
+  $id_atelier=$_POST['id_atelier'];
+  $sql ="UPDATE Ateliers SET name_atelier = '$nom', theme_atelier = '$theme' ,type_atelier = '$type',lieu_atelier ='$lieu',
+  duree_atelier = '$duree',horaire_atelier ='$horaire',capacite ='$capacite' ,inscription = '$inscription'
+  WHERE id_atelier = '$id_atelier'";
+} else {
+  $duree .=':00';
+  $horaire .=':00';
+  $sql ="INSERT INTO Ateliers (id_atelier,name_atelier,theme_atelier,type_atelier,id_laboratoire,lieu_atelier,duree_atelier,date_atelier,horaire_atelier,capacite,inscription)
   VALUES ('$id_atelier','$nom','$theme','$type','$id_laboratoire','$lieu','$duree','$date','$horaire','$capacite','$inscription')";
+}
 
-  if(!$db->query($sql)){
-  	die("Message d'erreur : ".$db->error."<br>");
-  }
+if(!$db->query($sql)){
+  printf("Message d'erreur : %s \n", $db->error);
+}
 
-	$db->close();  // on ferme la connexion
-	header('Location:ListeAteliers.php');
+$db->close();  // on ferme la connexion
+header('Location: http://localhost/Project/listeAtelier.php');
 ?>
